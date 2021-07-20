d09b10bff4d40886ff84968ba7a56b7fa4eec1a0
Skrat0401 <robin.vogelhaus@googlemail.com>
2021-07-20 15:54:37 +0200

    vivado infrastructure axi

---
+++ vivado-essentials/fifo_8x2048/fifo_8x2048_stub.v
@@ -0,0 +1,27 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+// Date        : Tue Jun 22 15:21:37 2021
+// Host        : Robins-Pc running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               C:/Users/robin/Documents/Linux_Thesis/xillinux-eval-microzed-2.0d/vivado-essentials/fifo_8x2048/fifo_8x2048_stub.v
+// Design      : fifo_8x2048
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "fifo_generator_v13_2_5,Vivado 2019.2" *)
+module fifo_8x2048(clk, srst, din, wr_en, rd_en, dout, full, empty)
+/* synthesis syn_black_box black_box_pad_pin="clk,srst,din[7:0],wr_en,rd_en,dout[7:0],full,empty" */;
+  input clk;
+  input srst;
+  input [7:0]din;
+  input wr_en;
+  input rd_en;
+  output [7:0]dout;
+  output full;
+  output empty;
+endmodule
